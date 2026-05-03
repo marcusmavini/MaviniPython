000285e40927f5cb2b9ada1c89c29e18b00262c8
Marcus Mavini <marcus.mavini9@gmail.com>
2026-05-03 15:08:38 -0300

    Adicionando mais exercicios

---
+++ NewPython/ex003.py
@@ -0,0 +1,9 @@
+# Some dois números e mostre
+
+print('===DESAFIO 003===')
+n1 = int(input('Primeiro Número: '))
+n2 = int(input('Segundo Número: '))
+
+soma = n1 +n2
+
+print(f'A soma de {n1} e {n2} é: {soma}')
---
+++ NewPython/ex004.py
@@ -0,0 +1,13 @@
+# Informações de variável
+
+print('===DESAFIO 004===')
+
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É numérico? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('Está em maiúsculas? ', a.isupper())
+print('Está em minúsculas? ', a.islower())
+print('Está capitalizada? ', a.istitle())
